27882438704a06180035d58e64d66da16320a052
Jonathan David Medina Gomez <jonathanmedina1809@hotmail.com>
2019-04-03 12:56:48 -0600

    init project

---
+++ util/database.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('tienda_perrona', 'root', 'WannaCry1809.', {
+  dialect: 'mysql',
+  host: 'localhost',
+  logging: false
+});
+
+module.exports = sequelize;
